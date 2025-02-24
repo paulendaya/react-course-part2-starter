7d19537b8c578b710bb5dee21269ac53432f6876
Paul Endaya <endaya.paul@gmail.com>
2025-02-25 01:11:46 +0300

    startedd with the usePosts.ts hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
-import TodoList from './react-query/TodoList';
+import "./App.css";
+import PostList from "./react-query/PostList";
+
 function App() {
   return (
     <>
       <h1>React Starter Project</h1>
-      <TodoList />
+      <PostList />
     </>
   );
 }
