d8218660a6550d437198551010dda14198d335f2
Paul Endaya <endaya.paul@gmail.com>
2025-02-26 01:42:33 +0300

    started the filter of Posts  using React Query

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -2,21 +2,25 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
-    id: number;
+    id: number | undefined;
     title: string;
     body: string;
     userId: number;
   }
 
-const usePosts = () => {
-    const fetchPosts = () =>
+const usePosts = (userId: number | undefined) => {
+    const fetchPosts = (userId: number | undefined) =>
         axios
-          .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+          .get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+            params: {
+                userId,
+            },
+          })
           .then((res) => res.data);
 
     return useQuery<Post[], Error>({
-        queryKey: ["posts"],
-        queryFn: fetchPosts,
+        queryKey: ["posts", userId],
+        queryFn: () => fetchPosts(userId),
         staleTime: 10_000,
       });
 }
