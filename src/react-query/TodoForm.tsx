5a3391eb844825529fff7d36aaee2ecdfd91ae59
Paul Endaya <paul@boxonvision.com>
2025-03-08 00:09:15 +0300

    implemented an optimistic update

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,61 +1,84 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useRef } from 'react';
-import { Todo } from './hooks/useTodos';
-import axios from 'axios';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
+
+interface AddTodoContext {
+  previousTodos: Todo[];
+}
 
 const TodoForm = () => {
   const queryClient = useQueryClient(); // Access the queryClient
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
+
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        newTodo,
+        ...(todos || []),
+      ]);
+      //clear the ref after the success mutation
+      if (ref.current) {
+        ref.current.value = "";
+      }
+
+      return { previousTodos };
+    },
     onSuccess: (savedTodo, newTodo) => {
       // console.log(savedTodo)
-      
+
       // First Approach: invalidating the cache
       /* queryClient.invalidateQueries({
         queryKey: ['todos']
-      }) // doesn't work in JSON placeholder */ 
+      }) // doesn't work in JSON placeholder */
 
       // Second Approach: Updating the data in the cache
-      queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])]) 
-      //clear the ref after the success mutation
-      if (ref.current) {
-        ref.current.value = '';
-      }
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+    },
+    onError: (error, newTodo, context) => {
+      if (!context) return;
 
-    }
+      queryClient.setQueryData<Todo[]>(["todos"], context.previousTodos);
+    },
   });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
     <>
-      {addTodo.error && <div className="alert alert-danger">{addTodo.error.message}</div>}
-      <form className="row mb-3" onSubmit={(event) => {
-        event.preventDefault();
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
 
-        if (ref.current && ref.current.value)
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            userId: 1,
-            completed: false
-          })
-      }}>
+          if (ref.current && ref.current.value)
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              userId: 1,
+              completed: false,
+            });
+        }}
+      >
         <div className="col">
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary"
-            disabled={addTodo.isLoading}>
-            {addTodo.isLoading ? 'Adding...' : 'Add'}
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding..." : "Add"}
           </button>
         </div>
       </form>
     </>
-    
   );
 };
 
