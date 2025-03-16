8bf0537a26310b97a1ef7a2f5102b4c600648c37
Paul Endaya <endaya.paul@gmail.com>
2025-03-16 08:35:52 +0300

    implemented another Reducer - AuthReducer to LoginStatus

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import Counter from "./state-management/Counter";
+import LoginStatus from "./state-management/LoginStatus";
 import TaskList from "./state-management/TaskList";
 
 function App() {
-  return <TaskList />;
+  return <LoginStatus />;
 }
 
 export default App;
