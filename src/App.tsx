939c064961e596793f7a66d5d1f32cc965d20f7b
Paul Endaya <endaya.paul@gmail.com>
2025-03-19 01:23:20 +0300

    Added TasksProvider and custom hook for Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import { useReducer } from "react";
 import "./App.css";
 
 import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
+import TasksProvider from "./state-management/TasksProvider";
 import Welcome from "./state-management/Welcome";
-import TasksContext from "./state-management/contexts/tasksContext";
-import tasksReducer from "./state-management/reducers/tasksReducer";
 
 function App() {
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-
   // We wrap our components that need to access the state in a Context Provider
   return (
     <AuthProvider>
-      <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+      <TasksProvider>
         <NavBar />
         <Welcome />
         <HomePage />
-      </TasksContext.Provider>
+      </TasksProvider>
     </AuthProvider>
   );
 }
