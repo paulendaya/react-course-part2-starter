95a52b583a7641c7f276b328f110c1124eb0f3ae
Paul Endaya <paul@boxonvision.com>
2025-03-25 07:22:17 +0300

    tested the re-rendering of component on state change

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -6,7 +6,14 @@ const NavBar = () => {
   const { tasks } = useTasks();
   //const { user } = useAuth();
   const { user } = useAuthStore();
-  const { counter } = useCounterStore();
+  // const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+  // s is the state object and counter is the property defined in the interface
+  // We're only interested in the counter property
+  // When  counter changes, the component will re-render
+  // just testing the feature of state management system that a component only re-renders when the state changes
+
+  console.log("Re-rendering NavBar");
 
   return (
     <nav className="navbar d-flex justify-content-between">
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 interface CounterStore {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
@@ -9,8 +10,11 @@ interface CounterStore {
 const useCounterStore = create<CounterStore>((set) => ({
   // the arrow function returns an object
   counter: 0,
+  max: 5,
   increment: () => set((store) => ({ counter: store.counter + 1 })),
-  reset: () => set((store) => ({ counter: 0 })),
+  //reset: () => set((store) => ({ counter: 0 })),
+  reset: () => set((store) => ({ max: 10 })),
+  // just testing the feature of state management system that a component only re-renders when the state changes
 }));
 
 //the above code is equivalent to the below using the conventinal function syntax
