44c066d76d357fc20e873a28d96768b2fa0aab21
Paul Endaya <endaya.paul@gmail.com>
2025-03-28 15:16:49 +0300

    implemented passing data with route parameters

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
+import UserDetailPage from "./UserDetailPage";
 
 // In this file, we define the routes for our application
 // by using the createBrowserRouter function from react-router-dom
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
   { path: "/contact", element: <ContactPage /> },
+  { path: "/user/:id", element: <UserDetailPage /> },
 ]);
 
 export default router;
