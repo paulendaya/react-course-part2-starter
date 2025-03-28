38975e29323e9c7c36596571101f0e9b458b3b2f
Paul Endaya <endaya.paul@gmail.com>
2025-03-28 15:30:47 +0300

    Tested getting data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,12 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams(); // useParams hook returns an object with the params of the current route
+  const [searchParams, setSearchParams] = useSearchParams(); // useSearchParams hook returns an object with the search params or query stings of the current route
+  const location = useLocation(); // useLocation hook returns an object with the current location
+
+  console.log(params, searchParams.get("name"), location);
+
   return <p>User</p>;
 };
 
